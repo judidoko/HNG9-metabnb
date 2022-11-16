cbafedabb576534919d6de226b3903cb429c8e0a
judidoko <judidoko@gmail.com>
2022-11-16 12:45:41 +0100

    metabnb project:adding popup section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,29 +9,111 @@ import HeaderLogo from "../../Assets/imgs/metabnb-logo.JPG";
 
 function Header() {
   return (
-    <Navbar className="nav" bg="white" expand="lg">
-      <Container>
-        <Link to="#">
-          <img src={HeaderLogo} alt="" />
-        </Link>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="nav_item me-auto my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
-            <Link to="/">Home</Link>
-            <Link to="/placetostay">Place To Stay</Link>
-            <Link to="#">NFTs</Link>
-            <Link to="#">Community</Link>
-          </Nav>
-          <Form className="d-flex">
-            <Button className="btn_connect">Connect Wallet</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar className="nav" bg="white" expand="lg">
+        <Container>
+          <Link to="#">
+            <img src={HeaderLogo} alt="" />
+          </Link>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="nav_item me-auto my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Link to="/">Home</Link>
+              <Link to="/placetostay">Place To Stay</Link>
+              <Link to="#">NFTs</Link>
+              <Link to="#">Community</Link>
+            </Nav>
+            <Form className="d-flex">
+              <Button className="btn_connect">Connect Wallet</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      {/* connection Wallet Section */}
+      <button
+        type="button"
+        class="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+        data-bs-whatever="@mdo"
+      >
+        Open modal for @mdo
+      </button>
+      <button
+        type="button"
+        class="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+        data-bs-whatever="@fat"
+      >
+        Open modal for @fat
+      </button>
+      <button
+        type="button"
+        class="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+        data-bs-whatever="@getbootstrap"
+      >
+        Open modal for @getbootstrap
+      </button>
+
+      <div
+        class="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">
+                New message
+              </h1>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">
+              <form>
+                <div class="mb-3">
+                  <label for="recipient-name" class="col-form-label">
+                    Recipient:
+                  </label>
+                  <input type="text" class="form-control" id="recipient-name" />
+                </div>
+                <div class="mb-3">
+                  <label for="message-text" class="col-form-label">
+                    Message:
+                  </label>
+                  <textarea class="form-control" id="message-text"></textarea>
+                </div>
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" class="btn btn-primary">
+                Send message
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
--- src/pages/PlaceToStayPage.jsx
+++ src/pages/PlaceToStayPage.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Button } from "react-bootstrap";
 import placeToStayData from "../Assets/Data/placetostayData";
 import PlaceToStay from "../components/placeToStay/PlaceToStay";
+import { RiSoundModuleLine } from "react-icons/ri";
 
 const PlaceToStayPage = () => {
   return (
@@ -11,15 +12,18 @@ const PlaceToStayPage = () => {
           <div className="container">
             <Row>
               <Col lg="12">
-                <div className="d-flex align-items-center gap-2 mb-5 mt-5">
-                  <span className="d-flex align-item-center gap-2">
-                    <i className="ri-sort-asc"></i> Sort By
-                  </span>
-                  <select>
-                    <option>Select</option>
-                    <option value="low">Low to High</option>
-                    <option value="high">High to Low</option>
-                  </select>
+                <div className=" filters d-flex align-items-center justify-content-evenly gap-4 mb-5 mt-5">
+                  <span>Resturant</span>
+                  <span>Cottage</span>
+                  <span>Castle</span>
+                  <span>Fantest City</span>
+                  <span>Beach</span>
+                  <span>Caebins</span>
+                  <span>Off-grid</span>
+                  <span>Farm</span>
+                  <Button>
+                    Location <RiSoundModuleLine className="location_icon" />
+                  </Button>
                 </div>
               </Col>
 
